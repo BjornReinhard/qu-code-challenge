98bf0adaf37f14b352318dca8f0d2d0245e24288
Daniil Stepanov <s7work28@gmail.com>
2025-07-01 15:21:29 -0500

    refactor(server): improve API structure and add Vercel deployment support - Added API entry point and deployment configuration (vercel.json, api/index.ts) for Vercel support - Added home page based on README.MD file (public/README.html)

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 import jokeRoutes from "./routes/jokeRoutes";
 
 const app = express();
@@ -9,6 +10,11 @@ dotenv.config();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.get("/", (_req, res) => {
+  res.sendFile(path.join(__dirname, "../public/README.html"));
+});
 
 app.use("/api/jokes", jokeRoutes);
 
